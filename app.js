83595e28d3b1334b624d0aeb872eede2a38fdc20
Thomas Werner <tom.renrew@gmail.com>
2022-06-05 20:12:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -59,5 +59,5 @@ app.post("/", function (req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`WeatherApp listening on port ${port}`);
 });
